ee286b103dc3d36ddaa411ad807f811871678022
Adya Naufal Fikri <adyanaufalf@gmail.com>
2019-09-03 18:02:48 +0700

    adding landmark list and link to detail

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,32 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Adya Naufal Fikri on 03/09/19.
+//  Copyright © 2019 Adya Naufal Fikri. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
