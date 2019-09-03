a27ce718f33299faca4701861391d8b644c2a8bd
Adya Naufal Fikri <adyanaufalf@gmail.com>
2019-09-03 16:07:35 +0700

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Adya Naufal Fikri on 03/09/19.
+//  Copyright © 2019 Adya Naufal Fikri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
